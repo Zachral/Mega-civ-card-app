fa112a6e0dab1eebacd755e35370161fa565fb27
Daniel Söderberg <soderberg.danne@gmail.com>
2023-01-17 22:08:21 +0100

    started on the array to store the cards

---
+++ Main.java
@@ -0,0 +1,24 @@
+public class Main {
+
+    public static void main(String[] args){
+
+        //All 495 cards as an array
+        Card[] cards = new Card[]{
+            new Card("Cloth Making", "Orange ", 50, 0, 5, 0, 10, 0,
+             "Naval Warfare", 10, 1),
+            new Card("Sculpture", "Blue", 50, 5, 10, 0, 0, 0, 
+            "Architecture", 10, 1),
+            new Card("Mysticism", "Blue" + "Yellow", 50, 0, 5, 0, 0, 5, 
+            "Monument", 10, 1),
+            new Card("Urbanism", "Red", 50, 10, 0, 5, 0, 0, "Diplomacy", 10, 1),
+
+
+
+            //new Card("Cloth Making", "Orange", 50, 0, 5, 0, 10, 0, 1),
+            //new Card("Sculpture", "blue", 50, 5, 10, 0, 0, 0, 1),
+            //new Card("Mysticism", "Blue" + "Yellow", 50, 0, 5, 0, 0, 5, 1)
+            
+
+        }; 
+    }
+}
